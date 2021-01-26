8ff29d401449148abd3dd5dfd08e32371149d75e
Andre Servidoni <deko.puc@gmail.com>
2021-01-26 23:35:52 +0100

    [BuildingManager] Instantiate GameObject when left button mouse is clicked

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+    // 0 - left mouse button / 1 - right mouse button
+    private readonly int LEFT_MOUSE_BUTTON = 0;
+
+    [SerializeField]
+    private GameObject pfWoodHarvester = null;
+    
+    private Camera mainCamera = null;
+
+    private void Start() {
+        mainCamera = Camera.main;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON)) {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition() {
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0.0f;
+        return worldPosition;
+    }
+}
